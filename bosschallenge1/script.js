ac09b413c95cf7f1a64bd1e28fd3fe195b5a4563
abhishek-dev <enquireabhishek@gmail.com>
2022-11-14 13:05:37 +0530

    bosslevel challenge 1

--- bosschallenge1/script.js
+++ bosschallenge1/script.js
@@ -1,10 +1,25 @@
-var randomNo1 = Math.floor(Math.random() * 6) + 1;
+
+
+debugger 
+function result(){
+    // alert("hello");
+    var randomNo1 = Math.floor(Math.random() * 6) + 1;
 var randomNo2 = Math.floor(Math.random() * 6) + 1;
 
 var ans1 = 6 - randomNo1;
 var ans2 = 6 - randomNo2;
-
-function result(){
+    for(var i = 0; i < ans1; i++){
+        var dice1 = document.querySelectorAll("#circle")[i];
+        dice1.style.backgroundColor = "black";
+    }
+    
+    for(var j = 0; j < ans2; j++){
+        var dice2 = document.querySelectorAll("#circle2")[j];
+        dice2.style.backgroundColor = "black";
+    }
+    
+    document.querySelector(".p1").innerHTML = randomNo1;
+    document.querySelector(".p2").innerHTML = randomNo2;
     if(ans1 < ans2){
         document.querySelector("h1").innerHTML = "Player 1 Won";
     }
@@ -17,18 +32,7 @@ function result(){
 }
 
 
-for(var i = 0; i < ans1; i++){
-    var dice1 = document.querySelectorAll("#circle")[i];
-    dice1.style.backgroundColor = "black";
-}
 
-for(var j = 0; j < ans2; j++){
-    var dice2 = document.querySelectorAll("#circle2")[j];
-    dice2.style.backgroundColor = "black";
-}
-
-document.querySelector(".p1").innerHTML = randomNo1;
-document.querySelector(".p2").innerHTML = randomNo2;
 
 
 
