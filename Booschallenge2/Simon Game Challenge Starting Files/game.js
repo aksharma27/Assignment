2157373baa2f2529c056d95eee8e5af2bbfb46b6
abhishek-dev <enquireabhishek@gmail.com>
2022-11-22 20:00:08 +0530

    updated nextSequence -> gamePatter.push

--- Booschallenge2/Simon Game Challenge Starting Files/game.js
+++ Booschallenge2/Simon Game Challenge Starting Files/game.js
@@ -11,11 +11,14 @@ let buttonColors = ["red", "blue", "green", "yellow"];
 
 
 function nextSequence() {
-    var randomNo = Math.floor(Math.random() * 3) + 1;
+    level ++;
+    $("#level-title").text("level "+ level);
+    let randomNo = Math.floor(Math.random() * 3) + 1;
     // console.log(randomNo)
     let randomChoosenColor = buttonColors[randomNo];
 
     gamePattern[length] = randomChoosenColor;
+    gamePattern.push(randomChoosenColor);
     $("#" + randomChoosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
     var audio = new Audio("sounds/" + randomChoosenColor + ".mp3");
     audio.play();
