678e47641a76b6f3dd689050732655f95997ca41
abhishek-dev <enquireabhishek@gmail.com>
2022-12-04 19:29:06 +0530

    made createNotes fun which gets all the props

--- KeeperApp/keeper/src/App.js
+++ KeeperApp/keeper/src/App.js
@@ -2,6 +2,17 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Note from './components/Note';
+import notes from './Notes';
+
+
+// console.log(notes);
+function createNotes(noteItem){
+  return 
+  <Note 
+    title = {noteItem.title}
+    content = {noteItem.content}
+  />
+}
 
 function App() {
   return (
